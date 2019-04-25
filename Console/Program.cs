6a8fd15489d2b93000bcb5aabe7d9d05614c709a
Felippe Carmello <46504403+fecarmello@users.noreply.github.com>
2019-04-25 17:58:27 -0300

    Add project

---
+++ Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var service = new TestSevice())
+            {
+                ServiceBase.Run(service);
+            }
+        }
+    }
+}
---
+++ Console/TestSevice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    internal class TestSevice : ServiceBase
+    {
+        public TestSevice()
+        {
+            ServiceName = "TestService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string filename = CheckFileExists();
+            File.AppendAllText(filename, $"{DateTime.Now} started.{Environment.NewLine}");
+
+            Run();
+        }
+
+        protected override void OnStop()
+        {
+            string filename = CheckFileExists();
+            File.AppendAllText(filename, $"{DateTime.Now} stopped.{Environment.NewLine}");
+        }
+
+        private static string CheckFileExists()
+        {
+            string filename = @"c:\tmp_file\NetCoreService.txt";
+            if (!File.Exists(filename))
+            {
+                File.Create(filename);
+            }
+
+            return filename;
+        }
+
+        public Task Run()
+        {
+            return Task.Run(() =>
+            {
+                while (true)
+                {
+                    string filename = CheckFileExists();
+                    File.AppendAllText(filename, $"{DateTime.Now} active.{Environment.NewLine}");
+
+                    Thread.Sleep(2000);
+                }
+            });
+        }
+    }
+}
